8acb0eaeeda9950691e4a8fde9f06803aad40649
Håvard Moen <post@haavard.name>
2017-11-06 22:53:05 +0100

    add support for setting cookie secret and mysql password through environment variables

--- run.sh
+++ run.sh
@@ -1,4 +1,40 @@
 #!/bin/bash
 
+if [ -n "$COOKIE_SECRET_FILE" ]
+then
+  if [ -f "$COOKIE_SECRET_FILE" ]
+  then
+    COOKIE_SECRET="$(< $COOKIE_SECRET_FILE)"
+  fi
+fi
+
+if [ -n "$MYSQL_PASSWORD_FILE" ]
+then
+  if [ -f "$MYSQL_PASSWORD_FILE" ]
+  then
+    MYSQL_PASSWORD="$(< $MYSQL_PASSWORD_FILE)"
+  fi
+fi
+
+if [ -n "$COOKIE_SECRET" ]
+then
+  cat <<EOD >> /var/www/juleol/environments/production_local.yml
+engines:
+  session:
+      Cookie:
+        secret_key: ${COOKIE_SECRET}
+EOD
+fi
+
+if [ -n "$MYSQL_PASSWORD" ]
+then
+  cat <<EOD >> /var/www/juleol/environments/production_local.yml
+plugins:
+  DBIC:
+    default:
+      password: $MYSQL_PASSWORD
+EOD
+fi
+
 /usr/sbin/nginx
 exec /var/www/juleol/public/dispatch.fcgi
