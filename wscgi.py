dd819aff59c4ad22b91672c6f7d26bd753a366f7
Håvard Moen <post@haavard.name>
2020-04-12 07:50:59 +0200

    add proxyfix to solve issues running behind reverse proxy

--- wscgi.py
+++ wscgi.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env juleol
 
 import juleol
+from werkzeug.middleware.proxy_fix import ProxyFix
 
-application = juleol.create_app()
+application = ProxyFix(juleol.create_app(), x_for=1, x_proto=1, x_host=1, x_port=1)
 
 if __name__ == "__main__":
     application.run()
